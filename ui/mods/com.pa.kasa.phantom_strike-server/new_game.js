6fe79f97152926d91f3a50210635026bb5f1d136
kasa0246 <karoseria99@gmail.com>
2024-11-24 12:41:12 +0100

    Delete ui/mods/com.pa.kasa.phantom_strike-server directory

--- ui/mods/com.pa.kasa.phantom_strike-server/new_game.js
+++
@@ -1,6 +0,0 @@
-      _.defer(function () {
-        model.localChatMessage(
-          loc("!LOC:Phantom Strike"),
-          loc("!LOC:ON. Your commander receives an ability to build fake copies of itself.")
-        );
-      })

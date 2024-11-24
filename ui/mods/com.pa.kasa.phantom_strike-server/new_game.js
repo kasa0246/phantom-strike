ab99cfa2898a55949a47f3df64e10b88d6c40bfd
kasa0246 <karoseria99@gmail.com>
2024-11-24 12:33:54 +0100

    Add files via upload

---
+++ ui/mods/com.pa.kasa.phantom_strike-server/new_game.js
@@ -0,0 +1,6 @@
+      _.defer(function () {
+        model.localChatMessage(
+          loc("!LOC:Phantom Strike"),
+          loc("!LOC:ON. Your commander receives an ability to build fake copies of itself.")
+        );
+      })
